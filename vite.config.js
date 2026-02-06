99d88076fcf9fab769e983dd97322219bfeba18c
김득우(Kim <dw.kim@gsenc.com>
2026-02-06 16:55:19 +0900

    fix: set vite base for github pages2

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 
 // GitHub Pages 배포 시, 정적 자산 경로가 /REPO_NAME/ 아래로 잡혀야 흰 화면(404)이 안 납니다.
 export default defineConfig({
-  base: "/file-lister/",
+  base: "file-lister",
 });
